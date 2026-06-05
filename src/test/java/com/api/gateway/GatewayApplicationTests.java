861ed498bed99a6bed447eb7f79858a19bdfb437
hendafarhani <farhanii.henda@gmail.com>
2026-06-05 13:26:06 +0200

    test: add integration tests for gateway discovery service configuration

--- src/test/java/com/api/gateway/GatewayApplicationTests.java
+++ src/test/java/com/api/gateway/GatewayApplicationTests.java
@@ -1,13 +1,32 @@
 package com.api.gateway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class GatewayApplicationTests {
 
+	@Value("${eureka.client.serviceUrl.defaultZone}")
+	private String eurekaDefaultZone;
+
+	@Value("${spring.cloud.gateway.discovery.locator.enabled}")
+	private boolean discoveryLocatorEnabled;
+
+	@Value("${spring.cloud.gateway.discovery.locator.lower-case-service-id}")
+	private boolean lowerCaseServiceId;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void gatewayUsesDiscoveryServicePortAndDiscoveryLocator() {
+		assertThat(eurekaDefaultZone).isEqualTo("http://discovery:8082/eureka");
+		assertThat(discoveryLocatorEnabled).isTrue();
+		assertThat(lowerCaseServiceId).isTrue();
 	}
 
 }
